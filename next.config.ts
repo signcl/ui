b5ce4160dafcecf2b099db21118bb8299a66e03e
sparanoid <t@sparanoid.com>
2025-02-27 00:56:51 -0800

    fix: storybook config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: '/storybook/',
+        destination: '/storybook/index.html',
+      },
+    ]
+  },
   // https://github.com/shadcn-ui/registry-template/blob/main/next.config.ts
   outputFileTracingIncludes: {
     components: ['./components/**/*'],
