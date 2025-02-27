4a5fc4ff707fb8a5a67fa58978712f6b89031484
sparanoid <t@sparanoid.com>
2025-02-27 01:20:20 -0800

    chore: update theme

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,5 +1,5 @@
 import { addons } from '@storybook/manager-api'
-import theme from './theme-bayes-ui'
+import theme from './theme'
 
 addons.setConfig({
   theme,
--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming'
 
 export default create({
   base: 'light',
-  brandTitle: 'Bayes UI',
+  brandTitle: 'signcl/ui',
   // brandUrl: 'https://example.com',
   // brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
   brandImage: 'https://openbayes.com/img/press/logo.svg',
