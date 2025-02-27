1bc3414944916fd180574d31cbd8ce597fc02cae
sparanoid <t@sparanoid.com>
2025-02-27 00:09:46 -0800

    chore: udpate next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  // https://github.com/shadcn-ui/registry-template/blob/main/next.config.ts
+  outputFileTracingIncludes: {
+    registry: ['./registry/**/*'],
+  },
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default nextConfig
