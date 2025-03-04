29a655e97b26dfb51c1ff5abb36e336b6203afd3
sparanoid <t@sparanoid.com>
2025-03-04 01:17:31 -0800

    feat: add toggle component

---
+++ components/ui/toggle.stories.tsx
@@ -0,0 +1,75 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { IconMicrophone } from '@tabler/icons-react'
+
+import { Toggle } from './toggle'
+
+const meta: Meta<typeof Toggle> = {
+  title: 'UI/Toggle',
+  component: Toggle,
+  tags: ['autodocs'],
+  args: {
+    children: 'Toggle',
+  },
+  parameters: {
+    layout: 'centered',
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof Toggle>
+
+export const Default: Story = {}
+
+export const WithIcon: Story = {
+  args: {
+    'aria-label': 'Toggle microphone',
+    'children': (
+      <>
+        <IconMicrophone className='size-4' />
+        <span>Microphone</span>
+      </>
+    ),
+  },
+}
+
+export const IconOnly: Story = {
+  args: {
+    'aria-label': 'Toggle microphone',
+    'children': <IconMicrophone />,
+  },
+}
+
+export const Outlined: Story = {
+  args: {
+    variant: 'outline',
+    children: 'Outlined Toggle',
+  },
+}
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+    children: 'Small',
+  },
+}
+
+export const Large: Story = {
+  args: {
+    size: 'lg',
+    children: 'Large',
+  },
+}
+
+export const Pressed: Story = {
+  args: {
+    pressed: true,
+    children: 'Pressed State',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    children: 'Disabled',
+  },
+}
---
+++ components/ui/toggle.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
+import * as TogglePrimitive from '@radix-ui/react-toggle'
+
+import { cn } from '@/lib/cn'
+
+const toggleVariants = cva(
+  'inline-flex items-center justify-center rounded-md font-medium focus-ring hover:bg-fg/5 hover:text-ac disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-ac/10 data-[state=on]:text-ac [&_svg]:pointer-events-none [&_svg]:shrink-0 gap-1',
+  {
+    variants: {
+      variant: {
+        default: 'border border-transparent',
+        outline: 'border hover:bg-fg/5 hover:text-ac hover:border-fg/40 data-[state=on]:border-ac',
+      },
+      size: {
+        sm: 'text-xs px-[0.5em] py-[0.25em] rounded-sm',
+        default: 'text-base px-[0.625em] py-[0.25em]',
+        lg: 'text-lg px-[0.8em] py-[0.325em]',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+)
+
+const Toggle = React.forwardRef<
+  React.ElementRef<typeof TogglePrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> & VariantProps<typeof toggleVariants>
+>(({ className, variant, size, ...props }, ref) => (
+  <TogglePrimitive.Root ref={ref} className={cn(toggleVariants({ variant, size, className }))} {...props} />
+))
+
+Toggle.displayName = TogglePrimitive.Root.displayName
+
+export { Toggle, toggleVariants }
