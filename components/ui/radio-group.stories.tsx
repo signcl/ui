acac5779addf71b7aa1fc1fbed235529e2b816d5
sparanoid <t@sparanoid.com>
2025-03-18 06:54:47 -0700

    feat: add RadioGroup component and stories for enhanced UI options

---
+++ components/ui/radio-group.stories.tsx
@@ -0,0 +1,101 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Label } from '@/components/ui/label'
+
+import { RadioGroup, RadioGroupItem } from './radio-group'
+
+const meta = {
+  title: 'UI/Radio Group',
+  component: RadioGroup,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof RadioGroup>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Basic: Story = {
+  render: args => (
+    <RadioGroup {...args} defaultValue='option-one'>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-one' id='option-one' />
+        <Label htmlFor='option-one'>Option One</Label>
+      </div>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-two' id='option-two' />
+        <Label htmlFor='option-two'>Option Two</Label>
+      </div>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-three' id='option-three' />
+        <Label htmlFor='option-three'>Option Three</Label>
+      </div>
+    </RadioGroup>
+  ),
+}
+
+export const Horizontal: Story = {
+  render: args => (
+    <RadioGroup {...args} defaultValue='option-one' className='flex space-x-4'>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-one' id='h-option-one' />
+        <Label htmlFor='h-option-one'>Option One</Label>
+      </div>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-two' id='h-option-two' />
+        <Label htmlFor='h-option-two'>Option Two</Label>
+      </div>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-three' id='h-option-three' />
+        <Label htmlFor='h-option-three'>Option Three</Label>
+      </div>
+    </RadioGroup>
+  ),
+}
+
+export const Disabled: Story = {
+  render: args => (
+    <RadioGroup {...args} defaultValue='option-one' disabled>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-one' id='d-option-one' />
+        <Label htmlFor='d-option-one' className='text-fg/50'>
+          Option One
+        </Label>
+      </div>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-two' id='d-option-two' />
+        <Label htmlFor='d-option-two' className='text-fg/50'>
+          Option Two
+        </Label>
+      </div>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-three' id='d-option-three' />
+        <Label htmlFor='d-option-three' className='text-fg/50'>
+          Option Three
+        </Label>
+      </div>
+    </RadioGroup>
+  ),
+}
+
+export const WithDisabledItems: Story = {
+  render: args => (
+    <RadioGroup {...args} defaultValue='option-one'>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-one' id='di-option-one' />
+        <Label htmlFor='di-option-one'>Option One</Label>
+      </div>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-two' id='di-option-two' disabled />
+        <Label htmlFor='di-option-two' className='text-fg/50'>
+          Option Two (Disabled)
+        </Label>
+      </div>
+      <div className='flex items-center space-x-2'>
+        <RadioGroupItem value='option-three' id='di-option-three' />
+        <Label htmlFor='di-option-three'>Option Three</Label>
+      </div>
+    </RadioGroup>
+  ),
+}
---
+++ components/ui/radio-group.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import * as React from 'react'
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+import { IconCircle } from '@tabler/icons-react'
+
+import { cn } from '@/lib/cn'
+
+const RadioGroup = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(({ className, ...props }, ref) => {
+  return <RadioGroupPrimitive.Root className={cn('grid gap-2', className)} {...props} ref={ref} />
+})
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
+
+const RadioGroupItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        'peer bg-bg/10 size-4 cursor-pointer appearance-none rounded-full border',
+        'disabled:cursor-not-allowed disabled:opacity-50',
+        'data-[state=checked]:border-ac data-[state=checked]:bg-ac',
+        'focus-ring',
+        className
+      )}
+      {...props}
+    >
+      <RadioGroupPrimitive.Indicator className='text-bg flex items-center justify-center'>
+        <IconCircle className='size-2 fill-current' />
+      </RadioGroupPrimitive.Indicator>
+    </RadioGroupPrimitive.Item>
+  )
+})
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
+
+export { RadioGroup, RadioGroupItem }
