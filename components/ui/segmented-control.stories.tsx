ef35338d2f40d312796902d3cc038316966072ba
sparanoid <t@sparanoid.com>
2025-04-14 04:21:47 -0700

    feat: add SegmentedControl and SegmentedControlItem components with Storybook stories for various configurations

---
+++ components/ui/segmented-control.stories.tsx
@@ -0,0 +1,162 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { IconList, IconPhoto, IconSettings } from '@tabler/icons-react'
+
+import { SegmentedControl, SegmentedControlItem } from '@/components/ui/segmented-control'
+
+const meta = {
+  title: 'UI/Segmented Control',
+  component: SegmentedControl,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof SegmentedControl>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    type: 'single',
+    defaultValue: 'center',
+    children: (
+      <>
+        <SegmentedControlItem value='left'>Left</SegmentedControlItem>
+        <SegmentedControlItem value='center'>Center</SegmentedControlItem>
+        <SegmentedControlItem value='right'>Right</SegmentedControlItem>
+      </>
+    ),
+  },
+}
+
+export const Small: Story = {
+  args: {
+    type: 'single',
+    defaultValue: 'center',
+    size: 'sm',
+    children: (
+      <>
+        <SegmentedControlItem value='left'>Left</SegmentedControlItem>
+        <SegmentedControlItem value='center'>Center</SegmentedControlItem>
+        <SegmentedControlItem value='right'>Right</SegmentedControlItem>
+      </>
+    ),
+  },
+}
+
+export const Large: Story = {
+  args: {
+    type: 'single',
+    defaultValue: 'center',
+    size: 'lg',
+    children: (
+      <>
+        <SegmentedControlItem value='left'>Left</SegmentedControlItem>
+        <SegmentedControlItem value='center'>Center</SegmentedControlItem>
+        <SegmentedControlItem value='right'>Right</SegmentedControlItem>
+      </>
+    ),
+  },
+}
+
+export const DefaultTint: Story = {
+  args: {
+    type: 'single',
+    defaultValue: 'default',
+    tint: 'default',
+    children: (
+      <>
+        <SegmentedControlItem value='default'>Default</SegmentedControlItem>
+        <SegmentedControlItem value='option'>Option</SegmentedControlItem>
+      </>
+    ),
+  },
+}
+
+export const AccentTint: Story = {
+  args: {
+    type: 'single',
+    defaultValue: 'accent',
+    tint: 'accent',
+    children: (
+      <>
+        <SegmentedControlItem value='accent'>Accent</SegmentedControlItem>
+        <SegmentedControlItem value='option'>Option</SegmentedControlItem>
+      </>
+    ),
+  },
+}
+
+export const WithTextAndIcons: Story = {
+  args: {
+    type: 'single',
+    defaultValue: 'list',
+    children: (
+      <>
+        <SegmentedControlItem value='list'>
+          <IconList size='1rem' />
+          <span>List</span>
+        </SegmentedControlItem>
+        <SegmentedControlItem value='photo'>
+          <IconPhoto size='1rem' />
+          <span>Photos</span>
+        </SegmentedControlItem>
+        <SegmentedControlItem value='settings'>
+          <IconSettings size='1rem' />
+          <span>Settings</span>
+        </SegmentedControlItem>
+      </>
+    ),
+  },
+}
+
+export const WithIconsOnly: Story = {
+  args: {
+    type: 'single',
+    defaultValue: 'list',
+    children: (
+      <>
+        <SegmentedControlItem value='list'>
+          <IconList size='1rem' />
+        </SegmentedControlItem>
+        <SegmentedControlItem value='photo'>
+          <IconPhoto size='1rem' />
+        </SegmentedControlItem>
+        <SegmentedControlItem value='settings'>
+          <IconSettings size='1rem' />
+        </SegmentedControlItem>
+      </>
+    ),
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    type: 'single',
+    defaultValue: 'center',
+    children: (
+      <>
+        <SegmentedControlItem value='left'>Left</SegmentedControlItem>
+        <SegmentedControlItem value='center'>Center</SegmentedControlItem>
+        <SegmentedControlItem value='right' disabled>
+          Right
+        </SegmentedControlItem>
+      </>
+    ),
+  },
+}
+
+export const Controlled: Story = {
+  args: {
+    type: 'single',
+    value: 'option1',
+    onValueChange: value => console.log(`Selected: ${value}`),
+    children: (
+      <>
+        <SegmentedControlItem value='option1'>Option 1</SegmentedControlItem>
+        <SegmentedControlItem value='option2'>Option 2</SegmentedControlItem>
+        <SegmentedControlItem value='option3'>Option 3</SegmentedControlItem>
+      </>
+    ),
+  },
+}
---
+++ components/ui/segmented-control.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import * as React from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
+
+import { cn } from '@/lib/cn'
+
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
+
+const segmentedControlVariants = cva('bg-fg/5 gap-0', {
+  variants: {
+    size: {
+      sm: 'rounded-sm',
+      default: 'rounded-md',
+      lg: 'rounded-md',
+    },
+  },
+  defaultVariants: {
+    size: 'default',
+  },
+})
+
+const segmentedControlItemVariants = cva('data-[state=on]:bg-bg text-fg/60 hover:text-fg border', {
+  variants: {
+    tint: {
+      default: 'data-[state=on]:text-fg data-[state=on]:border-fg/20',
+      accent: 'data-[state=on]:bg-ac data-[state=on]:text-bg',
+    },
+  },
+  defaultVariants: {
+    tint: 'default',
+  },
+})
+
+type SegmentedControlProps = React.ComponentPropsWithoutRef<typeof ToggleGroup> & {
+  tint?: VariantProps<typeof segmentedControlItemVariants>['tint']
+}
+
+const SegmentedControlContext = React.createContext<{
+  tint?: VariantProps<typeof segmentedControlItemVariants>['tint']
+}>({})
+
+const SegmentedControl = React.forwardRef<React.ElementRef<typeof ToggleGroup>, SegmentedControlProps>(
+  ({ className, size, variant = 'default', tint = 'default', children, ...props }, ref) => (
+    <SegmentedControlContext.Provider value={{ tint }}>
+      <ToggleGroup
+        ref={ref}
+        className={cn(segmentedControlVariants({ size }), className)}
+        size={size}
+        variant={variant}
+        {...props}
+      >
+        {children}
+      </ToggleGroup>
+    </SegmentedControlContext.Provider>
+  )
+)
+
+SegmentedControl.displayName = 'SegmentedControl'
+
+type SegmentedControlItemProps = React.ComponentPropsWithoutRef<typeof ToggleGroupItem> & {
+  tint?: VariantProps<typeof segmentedControlItemVariants>['tint']
+}
+
+const SegmentedControlItem = React.forwardRef<React.ElementRef<typeof ToggleGroupItem>, SegmentedControlItemProps>(
+  ({ className, children, tint, ...props }, ref) => {
+    const context = React.useContext(SegmentedControlContext)
+    const itemTint = tint || context.tint
+
+    return (
+      <ToggleGroupItem
+        ref={ref}
+        className={cn(
+          segmentedControlItemVariants({
+            tint: itemTint,
+          }),
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </ToggleGroupItem>
+    )
+  }
+)
+
+SegmentedControlItem.displayName = 'SegmentedControlItem'
+
+export { SegmentedControl, SegmentedControlItem }
